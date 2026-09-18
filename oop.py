04d0a765f07aa85c2c58fdf1c8fd17385cf3041a
Ahitophel <peterogidi16@gmail.com>
2026-09-18 01:48:57 +0100

    New commit

--- oop.py
+++ oop.py
@@ -1,11 +1,19 @@
 class Dog:
+    def __init__(self,name,breed):
+            self.name = name
+            self.breed = breed
     def bark(self):
         print("Bark")
     
         
-dog1 = Dog()
+dog1 = Dog("Savata","Eskimo")
 dog1.bark()
+print(dog1.name)
+print(dog1.breed)
 
 
-dog2 = Dog()
+dog2 = Dog("Lala","German_Shephard")
 dog2.bark()
+print(dog2.name)
+print(dog2.breed)
+
