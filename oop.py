00db37e3f5943674c59ef60741fdce0d8bd733fd
Ahitophel <peterogidi16@gmail.com>
2026-09-18 01:56:33 +0100

    New commit

--- oop.py
+++ oop.py
@@ -1,18 +1,30 @@
+########## Class Section ##########
 class Dog:
-    def __init__(self,name,breed):
+    def __init__(self,name,breed,owner):
             self.name = name
             self.breed = breed
+            self.owner = owner
     def bark(self):
         print("Bark")
     
-        
-dog1 = Dog("Savata","Eskimo")
+class Owner:
+    def __inti__(self,name,address,contact_no):
+        self.name = name
+        self.address = address
+        self.contact_no = contact_no
+
+
+
+
+######### Main Code #########
+owner1 = Owner('Johnson','5,jj street','9045384538')
+dog1 = Dog("Savata","Eskimo",owner1)
 dog1.bark()
 print(dog1.name)
 print(dog1.breed)
 
-
-dog2 = Dog("Lala","German_Shephard")
+owner1 = Owner('Danny','4,red street','9045584558')
+dog2 = Dog("Lala","German Shephard")
 dog2.bark()
 print(dog2.name)
 print(dog2.breed)
