9b1c93d89740db4ed93eabb25436b61c92279709
Prime <peterogidi16@gmail.com>
2026-09-19 03:18:00 +0100

    new commit

---
+++ user3.py
@@ -0,0 +1,14 @@
+class User:
+    def __init__(self,username,email,password):
+        self.username = username
+        self._email = email
+        self.password = password
+        
+   ##Getter property##
+    @property
+    def email(self):
+        return self._email  
+
+user1 = User("Danny","dan@gmail.com","1212dr")
+
+print(user1.email)
