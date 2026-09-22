51d0dfbe28b4fe95c456920c3cc2c98a3b887c00
Prime <peterogidi16@gmail.com>
2026-09-22 01:49:17 +0100

    New commit

---
+++ .vscode/Abstraction.py
@@ -0,0 +1,15 @@
+class EmailService:
+    def _connect(self):
+        print("connecting to email server")
+        
+    def _authenticate(self):
+        print("Authenticating...")
+        
+    def send_email(self):
+        self._connect()
+        self._authenticate
+        print("sending email...")
+        self._disconnect()
+        
+    def _disconnect(self):
+        print("Disconnecting from email server...")
